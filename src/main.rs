d1ec3d1aef021ba3b84c12973eeb99ea21ebbf90
Alexander Nookinov <anookinov@gmail.com>
2021-08-28 17:02:42 +0700

    clone an Rc<T> increases the reference count

--- src/main.rs
+++ src/main.rs
@@ -93,7 +93,14 @@ fn main() {
     println!("CustomSmartPointer dropped before the end of main.");
 
     let a = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(RcNil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = RcCons(3, Rc::clone(&a)); // can also use a.clone() but it will perform deep copies
-    let c = RcCons(4, Rc::clone(&a));
-    println!("{:#?}, {:#?}", b, c);
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = RcCons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+        println!("{:#?}", c);
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    println!("{:#?}", b);
 }
