dcbaa8b1fb7b528b56080748f05d9f50fab997e6
Alexander Nookinov <anookinov@gmail.com>
2021-08-29 01:58:23 +0700

    add a reference from a child to its parent

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ use crate::RcList::{RcCons, RcNil};
 use crate::RcRefCellList::{RcRefCellCons, RcRefCellNil};
 use crate::CycleList::{CycleCons, CycleNil};
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 struct CustomSmartPointer {
     data: String,
@@ -80,6 +80,7 @@ impl CycleList {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -173,11 +174,19 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
