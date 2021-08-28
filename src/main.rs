4a1060fabed2084f61892a3392a71ae638f038d0
Alexander Nookinov <anookinov@gmail.com>
2021-08-28 16:17:00 +0700

    implicit deref coercions with functions and methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
 // Box implements Deref and Drop traits
 enum List {
     Cons(i32, Box<List>),
@@ -18,4 +40,21 @@ fn main() {
     println!("b = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *(y.deref())); // equal to assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m); // &MyBox<String> -> &String -> &str
+    hello(&(*m)[..]); // if Rust didn't implement deref coercion (Deref::deref is called multiple times)
 }
